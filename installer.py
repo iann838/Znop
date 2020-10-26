158c5281a6c6bedd580a217cc2016c20bf26c8be
Paaksing <paaksingtech@gmail.com>
2020-10-25 20:20:17 -0400

    Changed installer command

--- installer.py
+++ installer.py
@@ -1,6 +1,6 @@
 import os
 
-installer = 'pyinstaller --noconfirm --onefile --console --icon "C:/Users/Spanish/Desktop/Znop/favicon.ico" --name "Znop" --add-data "C:/Users/Spanish/Desktop/Znop/znop;znop/" --add-data "C:/Users/Spanish/Desktop/Znop/LICENSE;." --add-data "C:/Users/Spanish/Desktop/Znop/README.md;."  "C:/Users/Spanish/Desktop/Znop/znop/__main__.py"'
+installer = 'pyinstaller --noconfirm --onefile --console --icon "C:/Users/Spanish/Desktop/Znop/favicon.ico" --name "Znop" --add-data "C:/Users/Spanish/Desktop/Znop/znop;znop/" --add-data "C:/Users/Spanish/Desktop/Znop/LICENSE;." --add-data "C:/Users/Spanish/Desktop/Znop/README.md;."  "C:/Users/Spanish/Desktop/Znop/__main__.py"'
 
 if __name__ == "__main__":
     os.system(installer)
